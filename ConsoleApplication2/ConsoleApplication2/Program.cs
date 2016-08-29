4676b6561ee88226f2d8d72a5286d3433acd7a3d
RemeberMe <reallynolie@yahoo.com>
2016-08-29 10:31:02 -0700

    Added comments

--- ConsoleApplication2/ConsoleApplication2/Program.cs
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -1,13 +1,37 @@
-﻿using System;
+﻿/***********************************************************************
+* AUTHOR:			HAL FISHER
+* COURSE:			CS 155
+* SECTION:			Mon 5:30pm
+* HOMEWORK #:		1
+* PROJECT #:		1
+* LAST MODIFIED:	07.29.2016
+***********************************************************************/
+/**********************************************************************
+* PSEUDOCODE
+* 0.  START
+* 1.  WRITELINE text image
+* 2.  NEXTLINE pause display
+* 3.  END PROGRAM
+***********************************************************************/
+///////////////////////////////////////////////////////////////////////
+////////////////////////// START PROGRAM  /////////////////////////////
+///////////////////////////////////////////////////////////////////////
+// USING SECTION
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// SOLUTION NAME
 namespace ConsoleApplication2
 {
+
+    // CLASS NAME
     class Program
     {
+
+        // MAIN METHOD
         static void Main(string[] args)
         {
             Console.WriteLine("       #####   \n    A| --  / |A\n    V|  0  0 |V\n     |    >  |\n      |  -- |\n         -- ");
