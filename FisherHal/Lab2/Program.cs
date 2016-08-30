e9cca4bf44be0a76172c0184ef38f938276435b6
w7219231 <w7219231@STU_J_SP_16_42>
2016-08-29 21:06:21 -0700

    lab2

---
+++ FisherHal/Lab2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1;
+            Console.Write("Enter the amount of coupons you have won: ");
+            string userInput = Console.ReadLine();
+            n1 = Int32.Parse(userInput);
+            Console.WriteLine("The amount of candy bars is " + n1 / 10 + " and gumballs is " + (n1 % 10) / 3 );
+            Console.ReadLine();
+
+
+        }
+    }
+}
