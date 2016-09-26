86dc04814680ad3e40814b9059cc8daf9ca58ca5
halfisher1212 <reallynolie@yahoo.com>
2016-09-26 09:45:12 -0700

    Added comments

--- FisherHal/HW4Project1/MainWindow.xaml.cs
+++ FisherHal/HW4Project1/MainWindow.xaml.cs
@@ -1,4 +1,23 @@
-﻿using System;
+﻿/**********************************************************************
+ * Project:         Homework 4 Project 1
+ * File Name:		HW4Project1.java
+ * Programmer:		Hal Fisher
+ * Date Last Modified: 	09.23.2016
+ ***********************************************************************
+ * PROGRAM -  BMI Calculator
+ * FUNCTION - Calculate BMI based on height and weight with color rating 
+ * *********************************************************************
+ * Class Created Associations
+ * Button Private Variables:
+ *	double bmi; 
+ *	int height, weight;
+ * ---------------------------- -------------------------
+ * Algorithm
+ * 0. Start Program
+ * 1. DISPLAY inputs for height and weight and button to fascilitate calculation
+ * 2. CALC bmi when user presses button and display color depending on calculation
+ * *********************************************************************/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -25,17 +44,22 @@ namespace HW4Project1
             InitializeComponent();
         }
 
+        /**
+         * Precondition:  xaml contains fields for height and weight
+         *              button object, and canvas to display colors
+         * Postcondition: n/a
+         */
         private void button_Click(object sender, RoutedEventArgs e)
         {
             int weight = 0;
             int height = 0;
-            int bmi;
+            double bmi;
 
             weight = Convert.ToInt16(WeightTextBox.Text);
             height = Convert.ToInt16(HeightTextBox.Text);
             weight = weight * 720;
             height = height * height;
-            bmi = weight / height;
+            bmi = weight / (double)height;
             BMITextBox.Text = bmi + "";
 
             if (bmi < 15)
