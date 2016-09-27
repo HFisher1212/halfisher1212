dff445f7a8b73a1a236167771c9fc8d8253efb79
halfisher1212 <reallynolie@yahoo.com>
2016-09-26 21:18:07 -0700

    added arrays and method to coin toss

--- lab7/Program.cs
+++ lab7/Program.cs
@@ -19,9 +19,10 @@ namespace HW4Project2
     class Program
     {
         // PRIVATE CLASS VARIABLES
-        public const int flips = 10000;
+        public const int FLIPS = 10000;
         private static int h = 0;
         private static int t = 0;
+        public enum Flips { Heads, Tails };
 
         /* MAIN SECTION */
 
@@ -34,12 +35,16 @@ namespace HW4Project2
             // MAIN BODY
             int num;
             Random generator = new Random();
+            int[] array = new int[10000];
 
-            for (int i = 1; i <= flips; i++)
+            for (int i = 0; i < FLIPS; i++)
             {
                 num = generator.Next(2);
+                array[i] = num;
+
                 if (num == 1)
                 {
+
                     h++;
                 }
                 else
@@ -47,11 +52,17 @@ namespace HW4Project2
                     t++;
                 }
             }
+
+            PrintOutput(array);
+        }
+
+        static void PrintOutput(int[] array)
+        { 
             Console.WriteLine();
             Console.WriteLine("Number of heads: " + h);
             Console.WriteLine("Number of tails: " + t);
-            Console.WriteLine("Percentage heads: {0:P2}%", h / (double)flips);
-            Console.WriteLine("Percent tails: {0:P2}%", t / (double)flips);
+            Console.WriteLine("Percentage heads: {0:P2}%", h / (double)FLIPS);
+            Console.WriteLine("Percent tails: {0:P2}%", t / (double)FLIPS);
             Console.ReadKey();
         }
     }
