e9e48a0688abf0bb91a6425d04d7ea513593c70c
halfisher1212 <reallynolie@yahoo.com>
2016-10-06 13:26:08 -0700

    Added Lec6 Lab2  I hope this is correct.  I attempted to recreate both labs and going back through them helped me to learn the lesson better

---
+++ Lec6Lab2/Fraction.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec6Lab2
+{
+    class Fraction
+    {
+        int numerator;
+        int denominator;
+
+        // CONSTRUCTOR DEFAULT
+        public Fraction(int num, int denom)
+        {
+            this.setNumerator(num);
+            this.setDenominator(denom);
+        }
+
+        // SETTERS
+        public void setNumerator(int numerator)
+        {
+            this.numerator = numerator;
+        }
+
+        public void setDenominator(int denominator)
+        {
+            this.denominator = denominator;
+        }
+
+        // GETTERS
+        public int getDenominator()
+        {
+            return this.denominator;
+        }
+
+        public int getNumerator()
+        {
+            return this.numerator;
+        }
+
+        public double getValue()
+        {
+            return this.numerator / (double)this.denominator;
+        }
+
+        // OTHER METODS
+        public override string ToString()
+        {
+            return String.Format("{0}/{1} = {2}", this.numerator, this.denominator, getValue());
+        }
+
+        public string reduced()
+        {
+            return String.Format("{0}/{1}", this.numerator / gcd(), this.denominator / gcd());
+        }
+
+        public int gcd()
+        {
+            int factor = 1;
+            int max = 0;
+            if (numerator > denominator)
+            {
+                max = denominator;
+            }
+            else
+            {
+                max = numerator;
+            }
+
+            for (int i = 2; i <= max; i++)
+            {
+                if (numerator % i == 0 && denominator % i == 0)
+                {
+                    factor = i;
+                }
+            }
+            return factor;
+        }
+    }
+}
---
+++ Lec6Lab2/Program.cs
@@ -0,0 +1,34 @@
+﻿/**
+ * @author      Hal Fisher
+ * Class        CS155 Mon 5:30
+ * Project      Lec6 Lab2
+ * Date         10.03.2016
+ * ***********************************************
+ * Algorithm
+ * 1.   CREATE fraction object (contains numerator, denominator integers)
+ * 2.   CALC lcd
+ * 3.   DISPLAY lcd
+ * 3.   DISPLAY results of fraction
+ * 4.  END PROGRAM
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec6Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction frac1 = new Fraction(4, 16);
+            Console.WriteLine(frac1.reduced());
+            Console.WriteLine(frac1);
+            Console.ReadKey();
+        }
+    }
+}
