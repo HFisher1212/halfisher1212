9ada7788e30c4eaa0123e2d53fd275ff4ab175fe
halfisher1212 <reallynolie@yahoo.com>
2016-10-07 11:02:06 -0700

    Homework 6 counter with menu.  Learned a lot about how to use WPF when creating objects.

---
+++ HW6/Counter.cs
@@ -0,0 +1,90 @@
+﻿/** Define a class called Counter whose objects count things.
+ * An object of this class records a count that is a nonnegative
+ * integer.Include methods to set the counter to 0, to increase
+ * the count by 1, and to decrease the count by 1.  Be sure that
+ * no method allows the value of the counter to become negative.
+ * Include a getter method that returns the current count value
+ * and a method that outputs the count to the screen.There should 
+ * be no input method or other setter methods.  The only method 
+ * that can set the counter is the one that sets it to 0.  Include 
+ * a ToString methods and a Equals method.  Write a driver program 
+ * that include a menu to interactively test each method in your 
+ * program.  For example, the menu should have an option that test 
+ * each method and operates on 1 object for example you should be 
+ * able to select the increment method which would increment your 
+ * Counter.
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW6
+{
+    class Counter
+    {
+        // CLASS VARIABLES
+        private int count = 0;
+
+        // DEFAULT CONSTRUCTOR
+        public Counter()
+        {
+            count = 0;
+        }
+
+        // RESET METHOD
+        public void zeroCount()
+        {
+            count = 0;
+        }
+
+        // GETTER METHODS
+        public int getCount()
+        {
+            return count;
+        }
+
+        // SETTER METHODS
+
+        // ADDONECOUNT METHOD
+        // Precondition- Counter object must be instantiated
+        // Postcondition- Counter object incremented by 1
+        public void addOneCount()
+        {
+            count++;
+        }
+
+        // SUBTRACTONECOUNT METHOD
+        // Precondition- counter object must be instantiated
+        // Postcondition- counter object decremented by 1 unless zero
+        public void subtractOneCount()
+        {
+            if (count != 0)
+            {
+                count--;
+            }
+        }
+
+        // REQUIRED METHODS
+
+        // TOSTRING METHOD
+        // Precondition- counter object must be instantiated
+        // Postcondition- returns string containing counter variable
+        public override string ToString()
+        {
+            return "The Count is: " + count;
+        }
+
+        // EQUALS METHOD
+        // Precondition- counter object must be instantiated
+        // Postcondition- returns boolean true false when comparing other object
+        public bool Equals(Counter other)
+        {
+            return this.getCount() == other.getCount();
+        }
+    }
+}
+
---
+++ HW6/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿/**
+ * Assignment       Homework 6
+ * @author          Hal Fisher
+ * Class            CS155
+ * Due Date         10.10.2016
+ * *****************************************
+ * Program Description: Define a class called Counter whose objects count things.
+ *     Include single class object that is manipulated with setters to increment,
+ *     decrement, and reset to zero counter object from menu (in this case a real menu).
+ *     Counter can not go below zero.
+ *******************************************
+ * Algorithme
+ * 0.   START PROGRAM
+ * 1.   CREATE counter class object initialized to zero.
+ * 2.   PROVIDE menu to reset, increment, decrement single counter object
+ * 3.   DISPLAY counter object contents
+ * 00.  END PROGRAM
+ * *****************************************
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HW6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // INSTANTIATION 
+        Counter counter = new Counter();
+
+        // MAIN METHOD (DECLARED IN App.xaml)
+
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            
+        }
+        // RESET EVENT METHOD
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            counter.zeroCount();
+            textBox.Text = Convert.ToString(counter);
+        }
+        
+        // INCREMENT EVENT METHOD
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            counter.addOneCount();
+            textBox.Text = Convert.ToString(counter);
+        }
+
+        // DECREMENT EVENT METHOD
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            counter.subtractOneCount();
+            textBox.Text = Convert.ToString(counter);
+        }
+    }
+}
