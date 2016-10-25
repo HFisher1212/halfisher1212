7ec84f3a089786d7f38ce45cda560ff4ea8921c2
w7219231 <w7219231@STU_J_SP_16_33>
2016-10-24 19:58:09 -0700

    Change to Properties

---
+++ FisherHal/Lec8Lab1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec8Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction frac1 = new Fraction(4, 16);
+            Console.WriteLine(frac1.reduced());
+            Console.WriteLine(frac1);
+            Console.ReadKey();
+        }
+    }
+}
