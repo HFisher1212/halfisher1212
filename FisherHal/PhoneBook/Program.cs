b063e8ea897f0eadbf1095b2f7fe2fb70b5e90a4
halfisher1212 <reallynolie@yahoo.com>
2016-11-07 21:16:30 -0800

    Lecture 10 Lab

---
+++ FisherHal/PhoneBook/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec10Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool quit = false;
+
+            while (!quit)
+            {
+                Console.Write("\n To Enter a New Phone Number Press 1: \n To Delete Record Press 2: \n To Search Records Press 3: \n To Quit Press 'q': ");
+                string answer = Console.ReadLine();
+                if (answer.Equals("q") || answer.Equals("Q"))
+                {
+                    quit = true;
+                }
+                else if (answer.Equals("1"))
+                {
+                    Records.AddContent();
+                }
+                else if (answer.Equals("2"))
+                {
+                    Records.DeleteContent();
+                }
+                else if (answer.Equals("3"))
+                {
+                    Console.Write("\n The Phone Number is: {0} \n", Records.SearchContent());
+                }
+                else
+                {
+                    Console.WriteLine("\n ERROR: That Was Not a Valid Entry!");
+                }
+            }
+        }
+    }
+}
---
+++ FisherHal/PhoneBook/Records.cs
@@ -0,0 +1,70 @@
+﻿/**
+ * @authoer Hal Fisher
+ * Project  Lec10Lab
+ * Date     11.7
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec10Lab
+{
+    class Records
+    {
+        private static Dictionary<string, long> phoneBook = new Dictionary<string, long>();
+
+        public static void AddContent()
+        {
+            bool quit = false;
+
+            while (!quit)
+            {
+                Console.Write("\n Please Enter the Name or Press 'q' to Quit: ");
+                string name = Console.ReadLine();
+                if (name.Equals("q") || name.Equals("Q"))
+                {
+                    quit = true;
+                }
+                else
+                {
+                    Console.Write("\n Enter Phone Number: ");
+                    long number = Convert.ToInt32(Console.ReadLine());
+                    phoneBook.Add(name, number);
+                }
+            }
+        }
+
+        public static void DeleteContent()
+        {
+            Console.Write("\n Please Enter the Name or Press 'q' to Quit: ");
+            string name = Console.ReadLine();
+            if(phoneBook.ContainsKey(name))
+            {
+                phoneBook.Remove(name);
+            }
+            else
+            {
+                Console.WriteLine(" ERROR: Name Not Found.");
+            }
+        }
+
+        public static long SearchContent()
+        {
+            Console.Write("\n Please Enter the Name or Press 'q' to Quit: ");
+            string name = Console.ReadLine();
+            if (phoneBook.ContainsKey(name))
+            {
+                return phoneBook[name];
+            }
+            else
+            {
+                Console.WriteLine(" ERROR: Name Not Found.");
+                return -1;
+            }
+        }
+    }
+}
+
