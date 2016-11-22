6fa5238811290840971aa8d85a218709e590f86b
w7219231 <w7219231@STU_J_SP_16_10>
2016-11-21 20:27:10 -0800

    Lec12 Lab Happy Thanksgiving Kevin!

---
+++ FisherHal/Lec12Lab/ParameterDelegate.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec12Lab
+{
+    public delegate double ParaDelegate(int size = 5, params int[] num);
+}
---
+++ FisherHal/Lec12Lab/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec12Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParaDelegate delegator = MeanCalc;
+
+            int[] a = { 1, 2, 3, 4, 5 };
+            double result = delegator(4,a);
+
+            Console.Write(" Mean: {0}", result);
+
+            delegator = Total;
+
+            result = delegator(6,a);
+
+            Console.Write("\n Total: {0}", result);
+
+            Console.ReadKey();
+        }
+
+
+        public static double MeanCalc(int a, params int[] num)
+        {
+            double total = 0;
+            int count = 0;
+
+            foreach (int n in num)
+            {
+                total += n;
+                count++;
+            }
+            return total / count;
+        }
+
+        public static double Total(int a, params int[] num)
+        {
+            int total = 0;
+            foreach (int n in num)
+            {
+                total += n;
+            }
+            return total + a;
+        } 
+    }
+}
