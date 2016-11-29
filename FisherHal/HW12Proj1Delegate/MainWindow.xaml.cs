1b9ab089d0645a2a149c01915733ecdef6c143f0
halfisher1212 <reallynolie@yahoo.com>
2016-11-28 16:55:19 -0800

    Homework 12 Delegate but was unable to figure it out.

---
+++ FisherHal/HW12Proj1Delegate/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HW12Proj1Delegate
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ShapeDelegator delegator;
+        Shapes shape;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Square_Click(object sender, RoutedEventArgs e)
+        {
+            shape = new Shapes(Convert.ToInt32(textBox), Convert.ToInt32(textBox1));
+            delegator = Square;
+        }
+
+        private void Circle_Click(object sender, RoutedEventArgs e)
+        {
+            shape = new Shapes(Convert.ToInt32(textBox3), Convert.ToInt32(textBox2));
+            delegator = Circle;
+        }
+
+        private void Triangle_Click(object sender, RoutedEventArgs e)
+        {
+            shape = new Shapes(Convert.ToInt32(textBox4), Convert.ToInt32(textBox5));
+            delegator = Triangle;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            textBox6.Text = shape.delegate();
+        }
+    }
+}
---
+++ FisherHal/HW12Proj1Delegate/Shapes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW12Proj1Delegate
+{
+    class Shapes
+    {
+        public int Height { set; get; }
+        public int Width { set; get; }
+        public ShapeDelegator delegator;
+
+        public Shapes()
+        {
+            Height = 0;
+            Width = 0;
+        }
+
+        public Shapes(int a, int b)
+        {
+            Height = a;
+            Width = b;
+        }
+
+        public double Square()
+        {
+            return Height * Width;
+        }
+
+        public double Triangle(int a, int b)
+        {
+            return (a * b) / 2; 
+        }
+
+        public double Circle(int a, int b)
+        {
+            return (a / 2) * 3.14;
+        }
+
+    }
+
+}
